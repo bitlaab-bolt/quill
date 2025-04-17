9548a0c347134d7ce175bc53c3879c20ac18b712
Al Mehedi <leftoverm9@gmail.com>
2025-04-17 09:09:56 +0600

    adding build step for always execute from zig-out/bin dir

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,8 @@
 const std = @import("std");
 
-const quill = @import("quill");
-const Quill = quill.Quill;
-
-
 pub fn main() !void {
-    var gpa_mem = std.heap.DebugAllocator(.{}).init;
-    defer std.debug.assert(gpa_mem.deinit() == .ok);
-    const heap = gpa_mem.allocator();
-
-    try Quill.init(.Serialized);
-    defer Quill.deinit();
-
-    var db = try Quill.open(heap, "hello.db");
-    defer db.close();
-
     std.debug.print("Hello, World!\n", .{});
+
+    // Let's start from here...
+    
 }
