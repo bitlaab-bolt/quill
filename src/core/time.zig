3b06cadff83c59c16b33441ab5d9abbd3c6eba6a
Al Mehedi <leftoverm9@gmail.com>
2025-09-09 22:04:51 +0600

    changing timestamp into seconds

--- src/core/time.zig
+++ src/core/time.zig
@@ -5,4 +5,4 @@ const time = std.time;
 
 
 /// # Returns Present Time (`Epoch`) in Milliseconds
-pub fn timestamp() i64 { return time.milliTimestamp(); }
+pub fn timestamp() i64 { return time.timestamp(); }
