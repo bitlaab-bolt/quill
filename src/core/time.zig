3c213e192137cffd73cfd0b271a7c1aa0c7c7839
Al Mehedi <leftoverm9@gmail.com>
2025-09-10 13:20:22 +0600

    adding bucket functionality

--- src/core/time.zig
+++ src/core/time.zig
@@ -4,5 +4,8 @@ const std = @import("std");
 const time = std.time;
 
 
-/// # Returns Present Time (`Epoch`) in Milliseconds
+/// # Returns Present Time (`Epoch`) in Seconds
 pub fn timestamp() i64 { return time.timestamp(); }
+
+/// # Returns Present Time (`Epoch`) in Milliseconds
+pub fn msTimestamp() i64 { return time.milliTimestamp(); }
